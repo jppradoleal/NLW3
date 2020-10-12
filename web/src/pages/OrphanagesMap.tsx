d07b1725014703935f354006dedb8e8a0bac51fe
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2020-10-12 16:39:59 -0300

    map page styles done - map implemented

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
@@ -22,7 +25,19 @@ function OrphanagesMap() {
                 </footer>
             </aside>
 
-            <div></div>
+            <Map
+                center= {[-23.3017096,-46.0086603]}
+                zoom={15}
+                style={{
+                    width: "100%",
+                    height: "100%"
+                }} 
+            >
+                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+                <TileLayer 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
+                />
+            </Map>
 
             <Link className="create-orphanage" to="">
                 <FiPlus size={32} color="#FFF"/>
