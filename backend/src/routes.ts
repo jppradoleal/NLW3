32bfa16b0700b51b93d66c6ea331e4e2645894c9
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2020-10-13 17:02:08 -0300

    multer configured and image upload done

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
+
 import OrphanagesController from './Controllers/OrphanagesController'
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
-routes.post('/orphanages', OrphanagesController.create);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
 
 export default routes
