34d7dc7d3a5b304dadbbfede4705805cdf99e1a5
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2020-10-13 05:21:32 -0300

    made favicon - made _redirects for netlify - changed tilelayer provider because idk how mapbox billing works

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -33,10 +33,10 @@ function OrphanagesMap() {
                     height: "100%"
                 }} 
             >
-                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-                <TileLayer 
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                {/* <TileLayer 
                     url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
-                />
+                /> */}
             </Map>
 
             <Link className="create-orphanage" to="">
