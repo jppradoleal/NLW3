b34f2d1278c9dd892b1aff1befd0518f7efae43f
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2020-10-13 08:09:07 -0300

    added responsiveness

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -10,7 +10,7 @@ function Landing() {
   return (
     <div id="page-landing">
       <div className="content-wrapper">
-        <img src={logoImg} alt="Happy"/>
+        <img src={logoImg} alt="Happy" className="logoImg"/>
 
         <main>
           <h1>Leve felicidade para o mundo</h1>
