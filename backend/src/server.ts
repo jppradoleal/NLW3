f4e3e37ed586772179098f43b15c4e7562efbb77
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2020-10-13 18:00:09 -0300

    added cors and validation of fields

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 
 import 'express-async-errors';
 
@@ -14,6 +15,7 @@ import errorHandler from './errors/handler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
