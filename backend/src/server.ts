c5edeca309558aa294724e4ce64b61c23d0afefc
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2020-10-13 17:43:08 -0300

    Added exception handling

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import path from 'path';
+
+import 'express-async-errors';
+
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -7,12 +10,14 @@ dotenv.config();
 import './database/connection';
 
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Listening on ${8080}`);
