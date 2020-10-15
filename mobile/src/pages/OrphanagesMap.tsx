81cd9234f312a03366ac50632e06233410bbaf3c
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2020-10-15 17:45:27 -0300

    Added routing and Nunito font

---
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import {View, Text, Dimensions, TouchableOpacity, StyleSheet} from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Feather } from '@expo/vector-icons';
+
+import mapMarker from '../images/mapMarker.png';
+import { useNavigation } from '@react-navigation/native';
+
+export default function OrphanagesMap() {
+  const navigation = useNavigation();
+
+  function handleNavigateToOrphanageDetails() {    
+    navigation.navigate('OrphanageDetails');
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView 
+        style={styles.map} 
+        initialRegion={{
+          latitude: -23.3017096,
+          longitude: -46.0086603,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008
+        }}
+        provider={PROVIDER_GOOGLE}
+      >
+        <Marker 
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8,
+          }}
+          coordinate={{
+            latitude: -23.3017096,
+            longitude: -46.0086603,
+          }}
+        >
+          <Callout 
+            tooltip
+            onPress={handleNavigateToOrphanageDetails}
+          >
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar das Meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.footer}>
+          <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+
+          <TouchableOpacity 
+            style={styles.createOrphanageButton} 
+            onPress={() => {}}
+          >
+            <Feather name="plus" size={20} color="#fff"/>
+          </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height+35,
+  },
+
+  calloutContainer: {
+    width: 168  ,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
+
+  calloutText: {
+    fontFamily: 'Nunito_700Bold',
+    color: '#0089a5',
+    fontSize: 14,
+  },
+
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    elevation: 3,
+  },
+
+  footerText: {
+    fontFamily: 'Nunito_700Bold',
+    color: '#8fa7b3',
+  },
+
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
