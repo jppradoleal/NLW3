31e4dcf163c6d27d17a83b347fb886a75ab511fe
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2020-10-18 02:11:13 -0300

    made api integration on dashboard

--- web/src/pages/Dashboard/DashboardApproved.tsx
+++ web/src/pages/Dashboard/DashboardApproved.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Marker, TileLayer, Map } from 'react-leaflet';
 import { FiEdit3, FiTrash } from 'react-icons/fi';
 import { useHistory } from 'react-router-dom';
@@ -8,15 +8,29 @@ import happyMapIcon from '../../utils/mapIcon';
 import Sidebar from './Sidebar';
 
 import '../../styles/pages/dashboard.css';
+import api from '../../services/api';
+import Orphanage from '../Orphanage';
+
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
 
 export default function DashboardApproved() {
   const { token } = useContext(UserContext);
+  const [orphanages, setOrphanages ] = useState<Orphanage[]>([]);
   const history = useHistory();
 
   useEffect(() => {
     if(!token) {
       //history.push('/login');
     }
+
+    api.get('/orphanages').then(response => {
+      setOrphanages(response.data);
+    })
   })
 
   return (
@@ -25,44 +39,48 @@ export default function DashboardApproved() {
       <div className="orphanages-listing">
 
       {/* Cada card aprovado tem um mapa, um h1, um botão de editar, e um botão de deletar */}
-          <div className="card">
-            <header className="card-header">
-            <Map
-                center= {[-23.3017096,-46.0086603]}
-                zoom={15}
-                style={{
-                  width: "100%",
-                  height: "100%"
-                }}
-                dragging={false}
-                zoomControl={false}                
-              >
-                  {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-                <TileLayer 
-                  url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
-                />
-                <Marker 
-                  position={[-23.3017096,-46.0086603]}
-                  icon={happyMapIcon}
-                  interactive={false}
-                >
-                </Marker>
-              </Map>
-            </header>
-            <footer>
-              <h1>
-                Orphanage Name Approved
-              </h1>
-              <div className="card-button-group">
-                <button>
-                  <FiEdit3 size={24} color="#29B6D1" />
-                </button>
-                <button>
-                  <FiTrash size={24} color="#29B6D1" />
-                </button>
+          {orphanages.map(orphanage => {
+            return (
+              <div className="card" key={orphanage.id}>
+                <header className="card-header">
+                <Map
+                    center= {[orphanage.latitude,orphanage.longitude]}
+                    zoom={15}
+                    style={{
+                      width: "100%",
+                      height: "100%"
+                    }}
+                    dragging={false}
+                    zoomControl={false}                
+                  >
+                      {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+                    <TileLayer 
+                      url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
+                    />
+                    <Marker 
+                      position={[orphanage.latitude,orphanage.longitude]}
+                      icon={happyMapIcon}
+                      interactive={false}
+                    >
+                    </Marker>
+                  </Map>
+                </header>
+                <footer>
+                  <h1>
+                    {orphanage.name}
+                  </h1>
+                  <div className="card-button-group">
+                    <button>
+                      <FiEdit3 size={24} color="#29B6D1" />
+                    </button>
+                    <button>
+                      <FiTrash size={24} color="#29B6D1" />
+                    </button>
+                  </div>
+                </footer>
               </div>
-            </footer>
-          </div>
+            )
+          })}
       </div>
     </div>
   )
--- web/src/pages/Dashboard/DashboardPending.tsx
+++ web/src/pages/Dashboard/DashboardPending.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Marker, TileLayer, Map } from 'react-leaflet';
 import { FiArrowRight } from 'react-icons/fi';
 import { useHistory } from 'react-router-dom';
@@ -8,16 +8,34 @@ import happyMapIcon from '../../utils/mapIcon';
 import Sidebar from './Sidebar';
 
 import '../../styles/pages/dashboard.css';
+import api from '../../services/api';
+
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
 
 export default function DashboardPending() {
   const { token } = useContext(UserContext);
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
   const history = useHistory();
 
   useEffect(() => {
     if(!token) {
       //history.push('/login');
     }
-  })
+
+    (async function getOrphanages() {
+      const response = await api.get('orphanages/dashboard', {
+        headers: {
+          'Authorization': 'Bearer ' + String(token)
+        }
+      });
+      setOrphanages(response.data);
+    })();
+  }, [token])
 
   return (
     <div id="page-dashboard">
@@ -25,39 +43,43 @@ export default function DashboardPending() {
       <div className="orphanages-listing">
 
       {/* Cada card aprovado tem um mapa, um h1, um botão de editar, e um botão de deletar */}
-          <div className="card">
-            <header className="card-header">
-            <Map
-                center= {[-23.3017096,-46.0086603]}
-                zoom={15}
-                style={{
-                  width: "100%",
-                  height: "100%"
-                }}
-                dragging={false}
-                zoomControl={false}                
-              >
-                  {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-                <TileLayer 
-                  url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
-                />
-                <Marker 
-                  position={[-23.3017096,-46.0086603]}
-                  icon={happyMapIcon}
-                  interactive={false}
-                >
-                </Marker>
-              </Map>
-            </header>
-            <footer>
-              <h1>
-                Orphanage Name
-              </h1>
-              <button>
-                <FiArrowRight size={24} color="#29B6D1" />
-              </button>
-            </footer>
-          </div>
+          {orphanages.map(orphanage => {
+            return (
+              <div className="card" key={orphanage.id}>
+                <header className="card-header">
+                <Map
+                    center= {[orphanage.latitude,orphanage.longitude]}
+                    zoom={15}
+                    style={{
+                      width: "100%",
+                      height: "100%"
+                    }}
+                    dragging={false}
+                    zoomControl={false}                
+                  >
+                      {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+                    <TileLayer 
+                      url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
+                    />
+                    <Marker 
+                      position={[orphanage.latitude,orphanage.longitude]}
+                      icon={happyMapIcon}
+                      interactive={false}
+                    >
+                    </Marker>
+                  </Map>
+                </header>
+                <footer>
+                  <h1>
+                    {orphanage.name}
+                  </h1>
+                  <button>
+                    <FiArrowRight size={24} color="#29B6D1" />
+                  </button>
+                </footer>
+              </div>
+            )
+          })}
       </div>
     </div>
   )
